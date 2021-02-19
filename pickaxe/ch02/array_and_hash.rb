99fb9fe734f1ea324d59baa0fd4f990e70766239
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2021-02-19 22:45:20 +0900

    Update array_and_hash.rb

--- pickaxe/ch02/array_and_hash.rb
+++ pickaxe/ch02/array_and_hash.rb
@@ -1,10 +1,12 @@
+# array 초기화
 a = [1, 'cat', 3.14] # 세 개의 요소를 가지는 배열을 만든다.
 puts "The first element is #{a[0]}"
-# 세 번째 요소 초기화하기
 
+# 세 번째 요소 초기화하기
 a[2] = nil
 puts "The array is now #{a.inspect}"
 
+# %w를 이용한 문자열 배열 초기화
 a = ['ant', 'bee', 'cat', 'dog', 'elk']
 a[0] # => "ant"
 a[3] # => "dog"
@@ -13,6 +15,7 @@ a = %w{ant bee cat dog elk}
 a[0] # => "ant"
 a[3] # => "dog"
 
+# hash 초기화
 inst_section = {
   'cello' => 'string',
   'clarinet' => 'woodwind',
